4643a5a2ebc7d9d55e81a1fc86b932c7919eed88
James Goodman <jamesgoodman206@gmail.com>
2022-10-11 17:37:26 +0100

    completed objects

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,56 @@
 const createPerson = (name, age) => {
-  // your code here
+  const person = {
+    name: name,
+    age: age
+  }
+  return person;
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  // eslint-disable-next-line no-prototype-builtins
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => {
+    return person.age;
+    })
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  const totalAge = people.reduce((total, person) => total + person.age, 0);
+  return totalAge / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  const person = {
+    name,
+    age,
+    introduce(name) {
+      return `Hi ${name}, my name is ${this.name} and I am ${this.age}!`;
+    }
+  };
+  return person;
 };
 
 module.exports = {
